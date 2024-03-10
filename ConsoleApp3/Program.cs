55d60d67d9481a49024794a4f4841e19f2ecd221
alberthaciverdiyev1 <alberthaciverdiyev55@gmail.com>
2024-03-10 12:15:08 -0700

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,42 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter Number");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number != null)
+        {
+            if (number == 1)
+            {
+                Console.WriteLine($"{number} Sade.");
+            }
+            else if (number <= 0)
+            {
+                Console.WriteLine($"Sade ve Murekkeb eded anlayisi ancaq natural ededeler ucun kecerlidir");
+            }
+            else
+            {
+                for (int i = 2; i <= number; i++)
+                {
+                    if (number % i == 0)
+                    {
+                        Console.WriteLine($"{number} Murekkeb.");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{number} Sade.");
+                        break;
+                    }
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("I am eRRoR.");
+
+        }
+    }
+
+}
